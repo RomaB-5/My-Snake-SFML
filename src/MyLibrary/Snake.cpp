62905b20c726c6b7928b5c6eb7d466465b8540c8
Roman Buchko <romanbychko84@gmail.com>
2023-12-03 12:07:00 +0200

    updated Snake::move() method

--- src/MyLibrary/Snake.cpp
+++ src/MyLibrary/Snake.cpp
@@ -1,4 +1,5 @@
 #include "../../include/MyLibrary/Snake.h"
+#include <algorithm>
 
 void Snake::grow() {
 	this->body.push_back(P2DVec(
@@ -8,6 +9,16 @@ void Snake::grow() {
 }
 
 void Snake::move() {
-	int8_t x = this->direction.get_x();
-	int8_t y = this->direction.get_y();
+	int8_t dx = this->direction.get_x();
+	int8_t dy = this->direction.get_y();
+
+	// body movement
+	for (auto it = ++this->body.begin(); it != this->body.end(); it++) {
+		it->set_x(prev(it)->get_x());
+		it->set_y(prev(it)->get_y());
+	}
+
+	// head movement
+	this->head()->set_x(this->head()->get_x() + dx);
+	this->head()->set_y(this->head()->get_y() + dx);
 }
