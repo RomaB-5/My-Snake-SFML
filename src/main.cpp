608611e4ebe553f1eb08cb22246e81d9145277af
Roman Buchko <romanbychko84@gmail.com>
2023-12-03 08:51:27 +0200

    first push after building Cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <thread>
 
 int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+{   
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        sf::Event event;
+        sf::Clock clock; // starts the clock
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
+            sf::Time elapsed1 = clock.getElapsedTime();
+            std::cout << elapsed1.asSeconds() << std::endl;
+            clock.restart();
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
+
+    return 0;
 }
