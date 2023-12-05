82c822c7dccef929e132bb5991c51dfcf10b8e6f
Roman Buchko <romanbychko84@gmail.com>
2023-12-05 18:47:06 +0200

    Added comments for clarity

--- include/MyLibrary/Snake.h
+++ include/MyLibrary/Snake.h
@@ -6,10 +6,14 @@ class Snake {
 private:
 	std::list<sf::Vector2i> body;
 	sf::Vector2i direction;
+
 public:
 	std::list<sf::Vector2i>::iterator head() { return this->body.begin(); }
 
+	// increases snake size by one 
 	void grow(void);
 	
+	// moves each body cell on the place of the following cell
+	// than moves head in the (direction.x, direction.y) direction.
 	void move(void);
 };
